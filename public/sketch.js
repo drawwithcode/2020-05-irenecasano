5e763dcd9b5da3b7878526d314fcb9407a8b9153
irenecasano <37075084+irenecasano@users.noreply.github.com>
2020-12-01 18:18:16 +0100

    com

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,11 @@
 let socket = io();
 // let myColor = "white";
-// let cursors = [];
+// let myCubes = [];
 let sky;
+let planes;
+let randomPlane;
+let canvas1;
+let canvas2;
 
 socket.on("connect", newConnection);
 socket.on("mouseBroadcast", drawOtherMouse);
@@ -18,105 +22,202 @@ function newConnection() {
 function drawOtherMouse(data) {
 // fill(data.color);
 // image(contrail, data.x, data.y, 50, 48);
-push()
-fill(255, 255, 255, 50);
-rect(data.x, data.y +50, 7, 7)
-rect(data.x, data.y +60, 7, 7)
-pop()
+p2.push()
+p2.noStroke();
+p2.fill(255, 255, 255, 50);
+p2.rect(data.x, data.y, 7, 7)
+p2.rect(data.x, data.y + 10, 7, 7)
+p2.pop()
+p1.image(data.myPlane, data.x, data.y)
 }
 
-function preload(){
-sky = loadImage("./assets/images/back.jpeg");
-planep = loadImage("./assets/images/pp.png");
+// function drawPlane(data) {
+// p1.push()
+// p1.image(data.myPlane, data.x, data.y)
+// p2.pop()
+// }
 
-// cursors = [plane1, plane2, plane3, plane4, plane5, plane6, plane7, plane8, plane9]
+// planep = loadImage("./assets/images/pp.png");
+// contrail = loadImage("./assets/images/contrail.png");
 
-contrail = loadImage("./assets/images/contrail.png");
 
-  // myFont = loadFont('./assets/font/FreePixel.ttf');
-myFont2 = loadFont('./assets/font/FreePixel2.ttf');
+  let sketch1 = function(p1){
+    p1.preload = function() {
+            sky = p2.loadImage("./assets/images/back.jpeg");
+
+      plane1 = p1.loadImage("./assets/images/plane1.png");
+      plane2 = p1.loadImage("./assets/images/plane2.png");
+      plane3 = p1.loadImage("./assets/images/plane3.png");
+      plane4 = p1.loadImage("./assets/images/plane4.png");
+      plane5 = p1.loadImage("./assets/images/plane5.png");
+      plane6 = p1.loadImage("./assets/images/plane6.png");
+      plane7 = p1.loadImage("./assets/images/plane7.png");
+      plane8 = p1.loadImage("./assets/images/plane8.png");
+      plane9 = p1.loadImage("./assets/images/plane9.png");
+      planeA = p1.loadImage("./assets/images/planea.png");
+
+      planes = [plane2, plane3, plane4, plane5, plane6, plane7, plane8, plane9]
+      randomPlane = p1.random(planes);
+
+          myFont2 = p2.loadFont('./assets/font/FreePixel2.ttf');
+    };
+   p1.setup = function(){
+   	canvas1 = p1.createCanvas(p1.windowWidth, p1.windowHeight, 0, 0);
+    canvas1.position(0,0);
+    p2.imageMode(p2.CENTER);
+    p2.image(sky, p2.width/2, p2.height/2, p2.windowWidth, p2.windowHeight)
+    p2.push()
+    p2.textSize(20)
+    p2.textFont(myFont2)
+    p2.fill("white")
+    p2.text("leave a message for your loved one", 50, 50)
+    p2.textSize(12)
+    p2.text("(or for anyone watching)", 50, 70)
+    p2.pop();
+   }
+
+   p1.draw = function(){
+fly();
+
+   }
+}
+function fly() {
+  p1.clear();
+  p1.image(randomPlane, p1.mouseX, p1.mouseY);
+  // randomPlane.loadPixels();
+  // const image64 = randomPlane.canvas.toDataURL();
+  // console.log(image64);
+
+  // let message = {
+  //   myPlane: image64,
+  // };
+  // socket.emit("plane", planemessage);
 }
 
 
-function setup() {
-  createCanvas(windowWidth,windowHeight)
-  noStroke()
-  imageMode(CENTER)
-  image(sky, width/2, height/2, windowWidth, windowHeight)
+let sketch2 = function(p2) {
 
-  // let randomImg = random(cursors)
-  // cursor("./assets/images/pp.png");
- textSize(20)
- textFont(myFont2)
- text("leave a message for your loved one", 50, 50)
- textSize(12)
- text("(or for anyone watching)", 50, 70)
-}
+    p2.preload = function() {
+    }
 
-function draw() {
-  // put drawing code here
+p2.setup = function(){
+canvas2 = p2.createCanvas(p2.windowWidth, p2.windowHeight);
+canvas2.position(0,0);
 
 }
 
-function mouseDragged() {
+p2.draw = function(){
+contrail();
 
+}
+}
+
+function contrail() {
+if (p2.mouseIsPressed) {
   // fill(myColor);
   // translate(mouseX + 25, mouseY + 25);
   // let a = atan2(- 24, - 25);
   // rotate(a);
   // image(contrail, mouseX, mouseY, 37, 36)
-  fill(255, 255, 255, 200);
-  rect(mouseX , mouseY +50, 7, 7)
-  rect(mouseX, mouseY +60, 7, 7)
+  p2.noStroke();
+  p2.fill(255, 255, 255, 170);
+  p2.rect(p2.mouseX , p2.mouseY +20, 7, 7)
+  p2.rect(p2.mouseX, p2.mouseY +30, 7, 7)
   // cursor("./assets/images/plane1.png")
 
   let message = {
-    x: mouseX,
-    y: mouseY +60,
+    x: p2.mouseX,
+    y: p2.mouseY +30,
     // color: myColor,
   };
   socket.emit("mouse", message);
 }
+}
 
 
+let p1 = new p5(sketch1);
+let p2 = new p5(sketch2);
 
-// let socket = io();
-// // let myColor = "white";
-// let myCursor;
-// let myCubes = [];
+// p2.draw = function(){
+//   for(let i = 0; i < myCubes.length; i++) {
+//    myCubes[i].p2.run();
+//  }
+//  if (myCubes.length > 20) {
+//        myCubes.p2.splice(i, 10)
+
+//      }
 //
-// socket.on("connect", newConnection);
-// socket.on("mouseBroadcast", drawOtherMouse);
-// // socket.on("color", setColor);
+//
+// }
+// }
+//
+// function contrail() {
+// if (p2.mouseIsPressed) {
+//   // fill(myColor);
+//   // translate(mouseX + 25, mouseY + 25);
+//   // let a = atan2(- 24, - 25);
+//   // rotate(a);
+//   // image(contrail, mouseX, mouseY, 37, 36)
+//   p2.addCube();
+//   // cursor("./assets/images/plane1.png")
+//
+//   let message = {
+//     x: p2.mouseX,
+//     y: p2.mouseY +30,
+//     // color: myColor,
+//   };
+//   socket.emit("mouse", message);
+// }
+// }
+//
+// class Cube {
+// constructor(temp_x,temp_y) {
+//   this.x=temp_x;
+//   this.y=temp_y;
+// }
+//
+// display() {
+//   p2.push();
+//  p2.rect(mouseX , mouseY +20, 7, 7)
+//    p2.rect(mouseX, mouseY +30, 7, 7)
+//   p2.pop();
+// }
+//
+//
+// run() {
+//   this.p2.display();
+// }
+// }
+
+// cursor("./assets/images/plane7.png");
+
+// function mouseDragged() {
+//
+//   // fill(myColor);
+//   // translate(mouseX + 25, mouseY + 25);
+//   // let a = atan2(- 24, - 25);
+//   // rotate(a);
+//   // image(contrail, mouseX, mouseY, 37, 36)
+//   fill(255, 255, 255, 200);
+//   rect(mouseX , mouseY +50, 7, 7)
+//   rect(mouseX, mouseY +60, 7, 7)
+//   // cursor("./assets/images/plane1.png")
+//
+//   let message = {
+//     x: mouseX,
+//     y: mouseY +60,
+//     // color: myColor,
+//   };
+//   socket.emit("mouse", message);
+// }
+
+//
+
 //
 // // function setColor(assignedColor) {
 // //   myColor = assignedColor;
 // }
 
-
-//
-// function newConnection() {
-//   console.log("your id: " + socket.id);
-// }
-//
-// function drawOtherMouse(data) {
-// // fill(data.color);
-// // rect(data.x, data.y, 30, 20);
-// image(contrail, data.x, data.y, 25, 24);
-// // cursor("./assets/images/plane1.png")
-// }
-//
-// function preload(){
-//   plane1 = loadImage("./assets/images/plane1.png");
-//   contrail = loadImage("./assets/images/contrail.png");
-// }
-//
-// function setup() {
-//   createCanvas(windowWidth,windowHeight)
-//   background("lightskyblue");
-//   cursor("./assets/images/plane1.png");
-//   // cursor("./assets/images/plane1.png")
-// }
 //
 // function draw() {
 //
@@ -124,7 +225,7 @@ function mouseDragged() {
 //   myCubes[i].run();
 // }
 // if (myCubes.length > 20) {
-//       myCubes.splice(0, 10)
+//       myCubes.splice(i, 10)
 //     }
 //
 //
@@ -155,7 +256,8 @@ function mouseDragged() {
 //
 // display() {
 //   push();
-//  image(pi,this.x,this.y, 25, 24);
+//  rect(mouseX , mouseY +20, 7, 7)
+   //rect(mouseX, mouseY +30, 7, 7)
 //   pop();
 // }
 //
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ console.log("node is running!");
 
 let express = require("express");
 
-let socket = require('socket.io');
+let socket = require("socket.io");
 
 let app = express();
 
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 3000;
 
 let server = app.listen(port);
 
@@ -18,23 +18,129 @@ io.on("connection", newConnection);
 
 function newConnection(socket) {
   console.log("new connection: " + socket.client.id);
-  // let clientColor = getRandomColor();
-  // socket.emit("color", clientColor);
+
+
+  // let clientColor = getRandomColor()
+let clientPlane = getPlane()
+  // socket.emit("color", clientColor)
+  socket.emit("myPlane", clientPlane)
+  //sendo color to all the other clients
+  // socket.broadcast.emit("newPlayer", clientColor);
   socket.on("mouse", mouseMessage);
+    socket.on("plane", mouseMessage);
+  // socket.on("mouse2", mouseMessage2);
+
 
   function mouseMessage(dataReceived) {
   console.log(socket.client.id, dataReceived);
-  socket.broadcast.emit("mouseBroadcast", dataReceived);
+  socket.broadcast.emit("mouseBroadcast", dataReceived)
   }
+
+  // function mouseMessage2(dataReceived) {
+  // console.log(socket.client.id, dataReceived);
+  // socket.broadcast.emit("mouseBroadcast", dataReceived)
+  //
+  // }
 }
 
-
+function getPlane() {
+  let myPlane = console.log(Buffer.from("image64", 'base64').toString('ascii'))
+return myPlane}
 // function getRandomColor() {
-//   var letters = "0123456789ABCDEF";
-//   var color = "#";
-//   for (var i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
+// var letters = "0123456789ABCDEF";
+// var color = "#";
+// for (var i = 0; i<6; i++) {
+//   color += letters[Math.floor(Math.random() + 16)];
+// }
+// return color;
+// }
+
+// console.log("node is running!");
+//
+// let express = require("express");
+//
+// let socket = require('socket.io');
+//
+// let app = express();
+//
+// let port = process.env.PORT || 3000
+//
+// let server = app.listen(port);
+//
+// app.use(express.static("public"));
+//
+// let io = socket(server);
+//
+// io.on("connection", newConnection);
+//
+// function newConnection(socket) {
+//   console.log("new connection: " + socket.client.id);
+//   // let clientColor = getRandomColor();
+//   // socket.emit("color", clientColor);
+//
+//   let clientPlane = getRandomPlane();
+//
+//   socket.emit("randomPlane", clientPlane)
+//   socket.broadcast.emit("newPlayer", clientPlane);
+// // socket.emit('image', image.toString('base64')); // image should be a buffer
+//   socket.on("mouse", mouseMessage);
+//   socket.on("planepos", generateRandomImg);
+//
+//
+//   function mouseMessage(dataReceived) {
+//   console.log(socket.client.id, dataReceived);
+//   socket.broadcast.emit("mouseBroadcast", dataReceived);
 //   }
 //
-//   return color;
-// }
+//
+//
+//
+// //   socket.on('image', async image => {
+// //     // image is an array of bytes
+// //     const buffer = Buffer.from(image);
+// //     await fs.writeFile('/images/plane6', buffer).catch(console.error); // fs.promises
+// // });
+// //   }
+// //
+// // }
+// // document.getElementById('file').addEventListener('change', function() {
+// //
+// //   const reader = new FileReader();
+// //   reader.onload = function() {
+// //     const bytes = new Uint8Array(this.result);
+// //     socket.emit('image', bytes);
+// //   };
+// //   reader.readAsArrayBuffer(this.files[0]);
+// //
+// // }, false);
+//
+//
+// //
+// // function getRandomPlane() {
+// // let numbers = "23456789";
+// // let randomPlane = "plane";
+// // for (var i = 0; i < 1; i++) {
+// // randomPlane += numbers[Math.floor(Math.random() * 8)]
+// // }
+// // return randomPlane;
+// // }
+//
+//
+//
+// // function getRandomPlane() {
+// //
+// // var randomPlane = new Array();
+// // randomPlane[0] = "images/plane2.png";
+// // randomPlane[1] = "images/plane3.png";
+// // randomPlane[2] = "images/plane4.png";
+// // }
+//
+// // function getRandomColor() {
+// //   var letters = "0123456789ABCDEF";
+// //   var color = "#";
+// //   for (var i = 0; i < 6; i++) {
+// //     color += letters[Math.floor(Math.random() * 16)];
+// //   }
+// //
+// //   return color;
+// // }
